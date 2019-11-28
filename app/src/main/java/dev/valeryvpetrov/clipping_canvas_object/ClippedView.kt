614bce3be70748b0c824911ae025698d89cacec2
Valery Petrov <valeryvpetrov.dev@gmail.com>
2019-11-28 17:43:20 +0600

    Replace activity content view with ClippedView

---
+++ app/src/main/java/dev/valeryvpetrov/clipping_canvas_object/ClippedView.kt
@@ -0,0 +1,14 @@
+package dev.valeryvpetrov.clipping_canvas_object
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import kotlin.jvm.JvmOverloads
+
+class ClippedView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+}

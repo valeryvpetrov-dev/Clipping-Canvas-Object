04ce17b3d6a2a0f35c949bc39d738347a1cf1176
Valery Petrov <valeryvpetrov.dev@gmail.com>
2019-11-28 11:16:49 +0600

    Android project base

---
+++ app/src/main/java/dev/valeryvpetrov/clipping_canvas_object/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.valeryvpetrov.clipping_canvas_object
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

8af61c3ab38d9cf8281772fb3f40d3d59bc570bd
Valery Petrov <valeryvpetrov.dev@gmail.com>
2019-11-29 10:15:03 +0600

    Init paint and path

--- app/src/main/java/dev/valeryvpetrov/clipping_canvas_object/ClippedView.kt
+++ app/src/main/java/dev/valeryvpetrov/clipping_canvas_object/ClippedView.kt
@@ -1,6 +1,8 @@
 package dev.valeryvpetrov.clipping_canvas_object
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import kotlin.jvm.JvmOverloads
@@ -11,4 +13,13 @@ class ClippedView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+    private val paint = Paint()
+
+    private val path = Path()
+
+    init {
+        paint.isAntiAlias = true
+        paint.strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        paint.textSize = resources.getDimension(R.dimen.textSize)
+    }
 }

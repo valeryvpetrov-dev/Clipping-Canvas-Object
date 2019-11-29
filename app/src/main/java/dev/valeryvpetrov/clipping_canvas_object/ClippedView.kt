82a21507deb7fb09aa0c581f1ada5117ae3a7e4c
Valery Petrov <valeryvpetrov.dev@gmail.com>
2019-11-29 10:18:44 +0600

    Set up the shapes

--- app/src/main/java/dev/valeryvpetrov/clipping_canvas_object/ClippedView.kt
+++ app/src/main/java/dev/valeryvpetrov/clipping_canvas_object/ClippedView.kt
@@ -17,6 +17,17 @@ class ClippedView @JvmOverloads constructor(
 
     private val path = Path()
 
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
     init {
         paint.isAntiAlias = true
         paint.strokeWidth = resources.getDimension(R.dimen.strokeWidth)
